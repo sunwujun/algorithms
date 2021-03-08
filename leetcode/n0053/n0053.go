08618a4dcd883611e43d3df839ceeebd25ba13d4
sunwujun <1542470166@qq.com>
2021-03-08 09:31:34 +0800

    [53] 最大子序和

--- leetcode/n0053/n0053.go
+++ leetcode/n0053/n0053.go
@@ -1,1 +1,16 @@
 package n0053
+
+// [53] 最大子序和
+
+func maxSubArray(nums []int) int {
+	max := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i-1] > 0 {
+			nums[i] += nums[i-1]
+		}
+		if nums[i] > max {
+			max = nums[i]
+		}
+	}
+	return max
+}
--- leetcode/n0053/n0053_test.go
+++ leetcode/n0053/n0053_test.go
@@ -1,1 +1,37 @@
 package n0053
+
+import "testing"
+
+func Test_maxSubArray(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "t1",
+			args: args{[]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}},
+			want: 6,
+		},
+		{
+			name: "t2",
+			args: args{[]int{0}},
+			want: 0,
+		},
+		{
+			name: "t3",
+			args: args{[]int{-1}},
+			want: -1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxSubArray(tt.args.nums); got != tt.want {
+				t.Errorf("maxSubArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
